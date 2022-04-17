7be01fc32fe031735520e2dab664b940320283f8
unknown <ahmednahid1995@gmail.com>
2022-04-17 06:42:56 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Home/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import Footer from './components/Home/Footer/Footer';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
     <div>
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer style={{ backgroundColor: 'green' }}>
+            <p className='text-center text-light fst-italic'>© Markdown 2025. All rights reserved</p>
+        </footer>
+    );
+};
+
+export default Footer;
