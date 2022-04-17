15da38fec1661775b3c3124c58d0d35365793870
unknown <ahmednahid1995@gmail.com>
2022-04-17 07:26:01 +0600

    about component implimented

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <p>about</p>
+        <div className='container'>
+            <p>This is <strong>Nahid Ahmed</strong>.</p>
+            <p>My aim is to become a web developer. Currently I am learning web development under 'Programming Hero'. I know this journey will not be easy. So I have to work hard to achieve my goal. For that I need to maintain a routine and stay focused.</p>
         </div>
     );
 };
